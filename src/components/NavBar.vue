b32968c72f21976e19a5a0567d41fff5709f3f23
dwityamaudyalfira <76533748+dwityamaudyalfira@users.noreply.github.com>
2022-05-10 11:43:30 +0700

    Initial commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,57 @@
+<template>
+  <nav class="navbar navbar-expand-lg " >
+    <div class="container-fluid">
+      <img  src="../assets/logoquran.png" class="rounded float-end"  width = 70 height = 70/>
+
+      <div class ="judul justify-content-center">QUR'AN WEB</div>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/"></a>
+          </li>
+        </ul>
+
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+};
+</script>
+
+<style scoped>
+body {
+  padding: 0;
+  margin: 0;
+  background: #f2f6e9;
+}
+.navbar {
+  background: #4655b4;
+}
+.nav-link,
+.navbar-brand {
+  color: #fff;
+  cursor: pointer;
+}
+.nav-link {
+  margin-right: 1em !important;
+}
+.nav-link:hover {
+  color: #000;
+}
+.navbar-collapse {
+  justify-content: flex-end;
+}
+.judul {
+  text-align: center;
+  color: #f2f2f2;
+  font-size: 20px;
+  font-family: "Book Antiqua";
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Navbar from "../components/navbar.vue";
+import { onMounted } from "vue";
+
+onMounted(() => {
+  document.getElementsByTagName("body")[0].style.backgroundImage ="Url('src/assets/bg2.jpg')"
+  document.getElementsByTagName("body")[0].style.backgroundSize ="cover";
+});
+</script>
+
+<template>
+  <main>
+    <navbar/>
+    <div class = "title">
+      <h1> Selamat datang di Quran web, Silahkan pilih search atau juz  </h1>
+    </div>
+
+  </main>
+</template>
+
+<style>
+.title{
+  text-align: center;
+}
+</style>
